6d55dae0424bc1de83c730443381947115677d42
Vadim Golenko <udachin.vadim@gmail.com>
2019-09-16 23:05:28 +0300

    Release 0.0.1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const path = require('path');
 const pg = require('pg');
-const https = require('https');
+const http = require('http');
 const options = { cert: fs.readFileSync('/Users/' + process.env.USER + '/server.crt'), key: fs.readFileSync('/Users/' + process.env.USER + '/server.key')};
 
 const BlogRoute = require('./routes/blog')
@@ -18,12 +18,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public/build')));
 
-app.use('/blog', BlogRoute)
-app.use('/portfolio', PortfolioRoute)
-app.use('/contacts', ContactsRoute)
+app.use('/api/blog', BlogRoute)
+app.use('/api/portfolio', PortfolioRoute)
+app.use('/api/contacts', ContactsRoute)
 
-let server = https.createServer(options, app);
-server.listen(3000, function() {
+let server = http.createServer(app);
+server.listen(8080, function() {
     console.log("server running at https://localhost:3000/")
 });
 
